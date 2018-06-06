24b6dd57ba2ff31fc983afcabb58aa83945c2dd3
linyows <linyows@gmail.com>
2018-06-06 11:19:35 +0900

    bump version to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'linyows@gmail.com'
 license          'MIT'
 description      'Installs/Configures vault'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
